a9f15a6fc1bdb2c002b9460c78f977b11e7f17e6
Kevin Jordan <Kevinmauricejordan2005@gmail.com>
2025-05-04 20:27:14 +0200

    Textcomponment erstmal fertig

--- src/pages/Startseite.jsx
+++ src/pages/Startseite.jsx
@@ -3,13 +3,21 @@ import Textcomponent from "../components/Textcomponent/Textcomponent";
 import "./startseite.css";
 
 export default function Startseite() {
-	const textArray = ["Wir sind die Fachleute für Gebäudereinigung mit denn Aufgabengebieten Gebäudereinigung, Büroreinigung, Praxisreinigung, Baustellenreinigung und Grundreinigungen"];
+	const headline = "Herzlich Willkommen bei der Reinigungsfirma E&E Gebäudereinigung";
+	const textArray = [
+		"Wir sind die Fachleute für Gebäudereinigung mit denn Aufgabengebieten Gebäudereinigung, Büroreinigung, Praxisreinigung, Baustellenreinigung und Grundreinigungen",
+		"Wir nehmen uns Zeit für Sie und beraten Sie umfassend und kompetent ",
+		"Unsere oberste Priorität ist eine gründliche und professionelle Reinigung für maximale Kundenzufriedenheit."
+	];
+
 	return (
 		<div className="max-width-rapper-center">
 			<div className="max-width-rapper">
 				<section className="home-section-one">
 					<img src={Image} alt="Image" className="home-image-one" />
-					<Textcomponent headline="Wilkommen" text={textArray} />
+					<div className="textcoomponent-div">
+						<Textcomponent headline={headline} text={textArray} />
+					</div>
 				</section>
 			</div>
 		</div>
